8047c852b08e6ee02cfac76c20659131d1dde312
rish-codes <67182874+rish-codes@users.noreply.github.com>
2020-07-26 01:02:19 +0530

    Creates a line chart depicting participation

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','ufp.pt','ucdavis.edu','indiana.edu','uct.ac.za','columbia.edu','berkeley.edu','mac.com','mtu.edu','gmail.com'],
+['2005',57,10,11,12,14,13,12,12,11,10],
+['2006',25,16,11,10,5,5,6,4,1,2]
+];
